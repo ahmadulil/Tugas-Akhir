3e72c8c80a4f0d0157fa209af05d373f5395e734
ahmadulil <ahmadulil03@gmail.com>
2015-11-17 01:52:41 +0700

    Update untuk skema baru

--- code/sptblok.m
+++ code/sptblok.m
@@ -1,12 +1,18 @@
-function vblok = sptblok(nframe,ncurent,startf,endf,arrE,curentE)
-sum=0;
-nsum=0;
-    for i=startf(1):endf(1)
-        for j=startf(2):endf(2)
-            sum=sum+1;
-            nsum=nsum+spt(nframe,ncurent,i,j,arrE,curentE);
-        end
-    end
-vblok=nsum/sum;
+function vblok = sptblok(video,nframe,ncurent,startp,endp)
+% sum=0;
+% nsum=0;
+    % for i=startf(1):endf(1)
+        % for j=startf(2):endf(2)
+            % sum=sum+1;
+            % nsum=nsum+spt(nframe,ncurent,i,j,arrE,curentE);
+        % end
+    % end
+startf=ncurent-nframe+1;
+for i=startf:ncurent
+	[arrE(:,:,i),arrblok(i)]=wblok(read(video,i),startp,endp);
+	v(:,:)=var(arrE(:,:,i));
+end
+[n1,n2]=size(v);
+vblok=sum(sum(v))/(n1+n2);
 end
 
--- code/wblok.m
+++ code/wblok.m
@@ -1,12 +1,18 @@
-function eblok = wblok(img,startp,endp)
-    jum=0;
-    nsum=0;
-    for i=startp(1):endp(1)
-        for j=startp(2):endp(2)
-            nsum=nsum+Ew(img,i,j);
-            jum=jum+1;
-        end
-    end
-    eblok=nsum/jum;
+function [eblok,Ew] = wblok(img,startp,endp)
+    % jum=0;
+    % nsum=0;
+    % for i=startp(1):endp(1)
+        % for j=startp(2):endp(2)
+            % nsum=nsum+Ew(img,i,j);
+            % jum=jum+1;
+        % end
+    % end
+	[LL,LH,HL,HH]=dwt2(img(startp(1):endp(1),startp(2):endp(2),1),'db1');
+	[n1,n2]=size(LL);
+	HL1=HL^2;
+	LH1=LH^2;
+	HH1=HH^2;
+	Ew=HL1+LH1+HH1;
+	eblok=sum(sum(Ew))/(n1+n2);
 end
 
