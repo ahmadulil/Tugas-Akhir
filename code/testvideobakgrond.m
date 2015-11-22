bbce2273bc917d7e18e9b955916304e9ef699f14
ahmadulil <ahmadulil03@gmail.com>
2015-11-23 05:38:58 +0700

    Update fun

--- code/testvideobakgrond.m
+++ code/testvideobakgrond.m
@@ -1,6 +1,6 @@
 v=VideoReader('C:\Users\Asprak-69\Documents\Sisrek Ulil\Tugas-Akhir\video label\3\3.mp4');
 load video3.mat;
-%  for i=21:v.NumberOfFrame
+% for i=21:v.NumberOfFrame
     xfor=bakground(25,20,v,30);
     imwrite(xfor,'1.jpg');
     xfor=uint8(xfor);
@@ -9,4 +9,4 @@ load video3.mat;
     c=sum(sum(img{30}==1));
     b=sum(sum(a));
     d=b/c;
-%    end
+% end
