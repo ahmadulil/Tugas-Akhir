2f28ffe2daa036a3d8c802bd72736bb474c49e77
ahmadulil <ahmadulil03@gmail.com>
2015-11-24 06:40:29 +0700

    Update

--- code/openimage.m
+++ code/openimage.m
@@ -1,7 +1,7 @@
-srcFiles = dir('C:\Users\Asprak-69\Documents\Sisrek Ulil\Tugas-Akhir\video label\3\*.jpg');  % the folder in which ur images exists
+srcFiles = dir('C:\Users\Asprak-69\Documents\Sisrek Ulil\Tugas-Akhir\video label\FormatFactorynonfire_indoor_5~2\label\*.jpg');  % the folder in which ur images exists
 for i = 1 : length(srcFiles)
-    filename = strcat('C:\Users\Asprak-69\Documents\Sisrek Ulil\Tugas-Akhir\video label\3\',srcFiles(i).name);
+    filename = strcat('C:\Users\Asprak-69\Documents\Sisrek Ulil\Tugas-Akhir\video label\FormatFactorynonfire_indoor_5~2\label\',srcFiles(i).name);
     img{i} = imread(filename);
     img{i} = im2bw(img{i},0.9);
 end
-save('video3.mat','img');
+save('video.mat','img');
--- code/testvideobakgrond.m
+++ code/testvideobakgrond.m
@@ -1,12 +1,14 @@
-v=VideoReader('C:\Users\Asprak-69\Documents\Sisrek Ulil\Tugas-Akhir\video label\3\3.mp4');
-load video3.mat;
+v=VideoReader('C:\Users\Asprak-69\Documents\Sisrek Ulil\Tugas-Akhir\video label\FormatFactorynonfire_indoor_5~2\22.avi');
+load video.mat;
 % for i=21:v.NumberOfFrame
-    xfor=bakground(25,20,v,30);
+    xfor=bakground(100,20,v,30);
     imwrite(xfor,'1.jpg');
     xfor=uint8(xfor);
-    xfor(xfor==0)=20;
-    a=xfor==img{30};
-    c=sum(sum(img{30}==1));
+    xfor(xfor==1)=20;
+    imga=uint8(img{30});
+    imga(imga==0)=20;
+    a=imga==xfor;
+%     c=sum(sum(img{30}==1));
     b=sum(sum(a));
-    d=b/c;
+%     d=b/c;
 % end
